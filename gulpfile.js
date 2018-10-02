c9699ab9970a98aa385ce49839422a320068a4b4
Kevin Santos <kevinochieng548@gmail.com>
2018-10-02 23:10:06 +0300

    create task to log message

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+
+/*
+ -- TOP LEVEL FUNCTIONS --
+ gulp.task - define tasks
+ gulp.src - point to files to use
+ gulp.dest - points to folder to output
+ gulp.watch  - watch files and folders for changes
+ */
+
+// logs message
+gulp.task('message', function() {
+  return console.log('Gulp is running...');
+});
+gulp.task('default', function() {
+  return console.log('Gulp is running...');
+});
