0b6329fa8c4587d177a03ef745ed23d6540f6e48
Kevin Santos <kevinochieng548@gmail.com>
2018-10-04 18:56:26 +0300

    require gulp contact

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 
 /*
  -- TOP LEVEL FUNCTIONS --
